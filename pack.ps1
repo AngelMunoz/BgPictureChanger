28720de7dd6a3a8becbaa9aaa07b68a7621278b7
Angel D. Munoz <scythes.lol@gmail.com>
2020-10-23 13:55:13 -0600

    bump

--- pack.ps1
+++ pack.ps1
@@ -2,4 +2,4 @@
 
 dotnet publish -r linux-x64 --self-contained true /p:PublishReadyToRun=true /p:PublishSingleFile=true -o dist
 dotnet pack
-dotnet nuget push dist/BgPictureChanger.0.1.0.nupkg -k "$env:NUGET_API_KEY" --source https://api.nuget.org/v3/index.json
+dotnet nuget push dist/BgPictureChanger.0.2.0.nupkg -k "$env:NUGET_API_KEY" --source https://api.nuget.org/v3/index.json
